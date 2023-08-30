3c3edf72abec392d6491a4ed367cffcc287ee7dd
Havi <havi@Havis-MacBook-Air.local>
2023-08-30 15:15:15 -0400

    push

--- src/main/java/com/mockexam/MockExam/controller/QuestionController.java
+++ src/main/java/com/mockexam/MockExam/controller/QuestionController.java
@@ -2,7 +2,6 @@ package com.mockexam.MockExam.controller;
 
 import com.mockexam.MockExam.entities.Question;
 import com.mockexam.MockExam.entities.QuestionRepository;
-import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.annotation.PostConstruct;
 import java.util.Arrays;
 import java.util.List;
 
--- src/main/java/com/mockexam/MockExam/entities/Question.java
+++ src/main/java/com/mockexam/MockExam/entities/Question.java
@@ -1,12 +1,13 @@
 package com.mockexam.MockExam.entities;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.List;
 
 @Entity
